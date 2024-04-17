3606d56ffb0a969071a2544c8517fa9129eadeae
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2024-04-17 13:24:53 +0300

    `yazi`: tiny refactor

--- home-manager/programs/yazi.nix
+++ home-manager/programs/yazi.nix
@@ -39,18 +39,22 @@
 				{ mime = "image/*"; use = "image"; }
 				{ mime = "video/*"; use = "video"; }
 				{ mime = "application/pdf"; use = "pdf"; }
+
 				{ name = "*.mp3"; use = "audio"; }
 				{ name = "*.wav"; use = "audio"; }
 				{ name = "*.ogg"; use = "audio"; }
+
 				{ name = "*.odt"; use = "libreoffice"; }
 				{ name = "*.odp"; use = "libreoffice"; }
 				{ name = "*.ods"; use = "libreoffice"; }
+
 				{ name = "*.doc" ; use = "libreoffice"; }
 				{ name = "*.docx"; use = "libreoffice"; }
 				{ name = "*.ppt" ; use = "libreoffice"; }
 				{ name = "*.pptx"; use = "libreoffice"; }
 				{ name = "*.xls" ; use = "libreoffice"; }
 				{ name = "*.xlsx"; use = "libreoffice"; }
+
 				# Multiple openers for a single rule
 				#{ name = "*.html", use = [ "browser", "text" ] },
 			];
