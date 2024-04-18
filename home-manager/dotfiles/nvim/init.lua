62d50e5c55e2cebb8806caa801289cceb893f839
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2024-04-18 13:16:37 +0300

    `neovim`: enable flash highlight on yank

--- home-manager/dotfiles/nvim/init.lua
+++ home-manager/dotfiles/nvim/init.lua
@@ -1092,3 +1092,11 @@ highlight MyTodo guifg=black guibg=orange
 au VimEnter * syntax match MyTodo /TODO/
 ]])
 
+vim.api.nvim_create_autocmd('TextYankPost', {
+	desc = 'Flash highlight when yanking (copying) text',
+	group = vim.api.nvim_create_augroup('flash-highlight-yank', { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
