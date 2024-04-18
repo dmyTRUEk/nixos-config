cb754eb76d1a401b865edb7f37586b61c86c5278
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2024-04-18 13:03:50 +0300

    `anyrun`: extract `symbols.ron` into separate file

--- home-manager/programs/anyrun.nix
+++ home-manager/programs/anyrun.nix
@@ -2,6 +2,7 @@
 { inputs, pkgs, ... }: {
 	enable = true;
 	extraCss = import ./anyrun-theme.nix;
+	extraConfigFiles."symbols.ron".text = builtins.readFile ./anyrun.symbols.ron;
 	config = {
 		plugins = with inputs.anyrun.packages.${pkgs.system}; [
 			# An array of all the plugins you want, which either can be packages, or paths to the .so files
@@ -31,21 +32,4 @@
 		showResultsImmediately = true;
 		maxEntries = null;
 	};
-	extraConfigFiles."symbols.ron".text = ''
-		Config(
-			// The prefix that the search needs to begin with to yield symbol results
-			prefix: "",
-			// Custom user defined symbols to be included along the unicode symbols
-			symbols: {
-				"approximately equal to": "≈",
-				"...": "…",
-				"lorem ipsum": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-				"fumo happy round": "ᗜᴗᗜ",
-				"fumo happy triangle": "ᗜˬᗜ",
-				"fumo sad round": "ᗜ⁔ᗜ",
-				"fumo sad triangle": "ᗜ˰ᗜ",
-			},
-			max_entries: 10,
-		)
-	'';
 }
