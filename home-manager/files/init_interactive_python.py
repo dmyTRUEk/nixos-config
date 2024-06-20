6e294e4d9db16da8ead03f910ad98e683150a526
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2024-06-20 20:39:57 +0300

    `ipy`: refactor

--- home-manager/files/init_interactive_python.py
+++ home-manager/files/init_interactive_python.py
@@ -2,7 +2,6 @@
 
 from typing import Callable, Iterator
 
-import sys
 from sys import argv as cli_args
 
 from time import sleep
@@ -16,7 +15,7 @@ from pipe import (
 	chain as chain_,
 	chain_with as chain_with_,
 	dedup as dedup_,
-	# enumerate as enumerate_, # on main, but not in released version
+	enumerate as enumerate_,
 	filter as filter_leave_,
 	groupby as group_by_,
 	islice as slice_,
@@ -40,16 +39,19 @@ from pipe import (
 
 # abcdefghijklmnopqrstuvwxyz
 
-def at(l: list["T"], index: int) -> "T": # pyright: ignore[reportUndefinedVariable]
+def at(l: list['T'], index: int) -> 'T': # pyright: ignore[reportUndefinedVariable]
 	return l[index]
 
-def avg(l: list["T"]) -> "T": # pyright: ignore[reportUndefinedVariable]
+def avg(l: list['T']) -> 'T': # pyright: ignore[reportUndefinedVariable]
 	return sum(l) / len(l)
 
 def diff(a: int | float | complex, b: int | float | complex) -> float | complex:
 	return abs(a-b) / (0.5 * (a+b))
 
-def filter_remove(it: Iterator["T"], pred: Callable[["T"], bool]) -> Iterator["T"]: # pyright: ignore[reportUndefinedVariable]
+# def filter_leave(it: Iterator['T'], pred: Callable[['T'], bool]) -> Iterator['T']: # pyright: ignore[reportUndefinedVariable]
+# 	return (x for x in it if pred(x))
+
+def filter_remove(it: Iterator['T'], pred: Callable[['T'], bool]) -> Iterator['T']: # pyright: ignore[reportUndefinedVariable]
 	return (x for x in it if not pred(x))
 
 def frange(start: float, end: float, step: float):
@@ -58,16 +60,19 @@ def frange(start: float, end: float, step: float):
 		yield x
 		x += step
 
-def index_of_max(l: list["T"]) -> int: # pyright: ignore[reportUndefinedVariable]
+def index_of_max(l: list['T']) -> int: # pyright: ignore[reportUndefinedVariable]
 	return max(enumerate(l), key=lambda x: x[1])[0]
+
+def index_of_min(l: list['T']) -> int: # pyright: ignore[reportUndefinedVariable]
+	return min(enumerate(l), key=lambda x: x[1])[0]
 
 def lerp(a: int | float | complex, b: int | float | complex, t: float) -> float | complex:
 	return a*(1.-t) + b*t
 
-def split_at(l: list["T"], index: int) -> tuple[list["T"], list["T"]]: # pyright: ignore[reportUndefinedVariable]
+def split_at(l: list['T'], index: int) -> tuple[list['T'], list['T']]: # pyright: ignore[reportUndefinedVariable]
 	return ( l[:index], l[index:] )
 
-def split_at_percentage(l: list["T"], p: float) -> tuple[list["T"], list["T"]]: # pyright: ignore[reportUndefinedVariable]
+def split_at_percentage(l: list['T'], p: float) -> tuple[list['T'], list['T']]: # pyright: ignore[reportUndefinedVariable]
 	return split_at(l, round(len(l) * p))
 
 def std_dev(l: list[float]) -> float:
@@ -78,7 +83,7 @@ def unzip2(xy: Iterator) -> tuple[Iterator, Iterator]:
 	xy1, xy2 = itertools.tee(xy)
 	return ( (x for x, _ in xy1), (y for _, y in xy2) )
 
-def windows(it: Iterator["T"] | list["T"], window_size: int) -> Iterator[tuple["T", ...]] | list[tuple["T", ...]]: # pyright: ignore[reportUndefinedVariable]
+def windows(it: Iterator['T'] | list['T'], window_size: int) -> Iterator[tuple['T', ...]] | list[tuple['T', ...]]: # pyright: ignore[reportUndefinedVariable]
 	# simple impl for lists:
 	# assert window_size <= len(l)
 	# res = []
@@ -98,9 +103,10 @@ def windows(it: Iterator["T"] | list["T"], window_size: int) -> Iterator[tuple["
 # Pipes:
 at_ = Pipe(at)
 avg_ = Pipe(avg)
-enumerate_ = Pipe(enumerate)
+# filter_leave_ = Pipe(filter_leave) -> pipe.filter
 filter_remove_ = Pipe(filter_remove)
 index_of_max_ = Pipe(index_of_max)
+index_of_min_ = Pipe(index_of_min)
 list_ = Pipe(list)
 split_at_ = Pipe(split_at)
 split_at_percentage_ = Pipe(split_at_percentage)
@@ -115,6 +121,18 @@ def binomial(n: int, m: int) -> int:
 	return factorial(n) // (factorial(m) * factorial(n-m))
 
 
+def xor_encryptor_decryptor(password: str) -> Callable[[str], str]:
+	password = str(hash(password))
+	def xor_encrypt(text: str) -> str:
+		encrypted_l = []
+		for i in range(len(text)):
+			encrypted_c = ord(password[i%len(password)]) ^ ord(text[i])
+			encrypted_l.append(chr(encrypted_c))
+		return ''.join(encrypted_l)
+	return xor_encrypt
 
-if __name__ == "__main__":
-	print(f"Python {sys.version} on {sys.platform}")
+
+
+if __name__ == '__main__':
+	import sys
+	print(f'Python {sys.version} on {sys.platform}')
