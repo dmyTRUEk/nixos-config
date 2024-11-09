78220e45cba3b7d199ac4fb66ce1ddde476140e4
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2024-11-09 21:25:31 +0200

    refactor: move `stateVersion` to bottom of config file in home & os

--- home/home-common.nix
+++ home/home-common.nix
@@ -9,9 +9,6 @@
 }: {
 	# TODO?: make `enable`/`enabled` function that sets enable=true.
 
-	# https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-	home.stateVersion = "23.11";
-
 	imports = [
 		inputs.nix-colors.homeManagerModules.default
 
@@ -343,4 +340,13 @@
 			};
 		};
 	};
+
+	# This value determines the NixOS release from which the default
+	# settings for stateful data, like file locations and database versions
+	# on your system were taken. It‘s perfectly fine and recommended to leave
+	# this value at the release version of the first install of this system.
+	# Before changing this value read the documentation for this option
+	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+	# https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+	home.stateVersion = "23.11"; # Did you read the comment?
 }
--- os/configuration-common.nix
+++ os/configuration-common.nix
@@ -7,9 +7,6 @@
 	pkgs,
 	...
 }: {
-	# https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-	system.stateVersion = "23.11";
-
 	imports = [
 		# If you want to use modules from other flakes (such as nixos-hardware):
 		# inputs.hardware.nixosModules.common-cpu-amd
@@ -279,4 +276,13 @@
 			];
 		};
 	};
+
+	# This value determines the NixOS release from which the default
+	# settings for stateful data, like file locations and database versions
+	# on your system were taken. It‘s perfectly fine and recommended to leave
+	# this value at the release version of the first install of this system.
+	# Before changing this value read the documentation for this option
+	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+	# https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+	system.stateVersion = "23.11"; # Did you read the comment?
 }
