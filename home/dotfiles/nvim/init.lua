e1441fe688d42df1c52b2bbd2ab064c3a76e41de
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2024-12-15 19:03:52 +0200

    `neovim`: add `lean` plugin

--- home/dotfiles/nvim/init.lua
+++ home/dotfiles/nvim/init.lua
@@ -944,21 +944,21 @@ require('lazy').setup {
 	-- 	lazy = true,
 	-- },
 
-	-- {
-	-- 	'Julian/lean.nvim',
-	-- 	event = { 'BufReadPre *.lean', 'BufNewFile *.lean' },
-	-- 	dependencies = {
-	-- 		'neovim/nvim-lspconfig',
-	-- 		'nvim-lua/plenary.nvim',
-	-- 		-- you also will likely want nvim-cmp or some completion engine
-	-- 		'hrsh7th/nvim-cmp',
-	-- 	},
-	-- 	-- see details below for full configuration options
-	-- 	opts = {
-	-- 		lsp = {},
-	-- 		mappings = true,
-	-- 	}
-	-- }
+	{
+		'Julian/lean.nvim',
+		event = { 'BufReadPre *.lean', 'BufNewFile *.lean' },
+		dependencies = {
+			'neovim/nvim-lspconfig',
+			'nvim-lua/plenary.nvim',
+			-- you also will likely want nvim-cmp or some completion engine
+			'hrsh7th/nvim-cmp',
+		},
+		-- see details below for full configuration options
+		opts = {
+			lsp = {},
+			mappings = true,
+		}
+	},
 }
 
 
