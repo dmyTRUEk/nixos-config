17f4e28a1bc1d6db48b146e095ba29a3befa6618
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2025-11-11 22:14:22 +0200

    neovim: add keymaps to goto next/prev/show diagnostics

--- home/dotfiles/nvim/init.lua
+++ home/dotfiles/nvim/init.lua
@@ -1056,6 +1056,10 @@ require('lazy').setup({
 					--  the definition of its *type*, not where it was *defined*.
 					map('gt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
 
+					keymap_n('<c-h>', vim.diagnostic.goto_prev)
+					keymap_n('<c-l>', vim.diagnostic.goto_next)
+					keymap_n('<leader>d', vim.diagnostic.open_float)
+
 					-- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
 					---@param client vim.lsp.Client
 					---@param method vim.lsp.protocol.Method
