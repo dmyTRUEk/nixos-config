9259a44706facbced3535d64076269fd1f86bbcd
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2025-11-17 12:10:17 +0200

    os: set `QT_QPA_PLATFORMTHEME=kde` to fix cantor's colors/theme

--- os/configuration-common.nix
+++ os/configuration-common.nix
@@ -333,6 +333,10 @@
 		};
 	};
 
+	environment.sessionVariables = {
+		QT_QPA_PLATFORMTHEME = "kde"; # fix Cantor's colors/theme
+	};
+
 	# This value determines the NixOS release from which the default
 	# settings for stateful data, like file locations and database versions
 	# on your system were taken. It‘s perfectly fine and recommended to leave
